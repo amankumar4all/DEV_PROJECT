77b7f3ab387cd7d249e324626a70cef0df585611
AMAN KUMAR <34826492+amankumar4all@users.noreply.github.com>
2021-03-12 11:25:28 +0530

    Update wcat.js

--- wcat Functionality/wcat.js
+++ wcat Functionality/wcat.js
@@ -1,1 +1,20 @@
+let n =require("./commands/addNumberToLine");
 
+let input=process.argv.slice(2);
+
+let value1=input[0];
+switch(value1){
+    case "-n":
+        n.setNumber(input[0],input[1]);
+    case "-s":
+        n.setNumber(input[0],input[1]);
+    case "-b":
+        n.setNumber(input[0],input[1]);
+    default:
+        
+        let val=value1.split(".");
+        let extension=pathArray[val.length-1];
+        if(ext=="txt"){
+            
+        }
+}
